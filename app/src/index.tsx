1c62ed698a92e45bfddd7ba7d214565421df99b5
Manoj Khanna <manojkhannakm@gmail.com>
2017-07-25 13:31:53 +0530

    Added app

---
+++ app/src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './index.css';
+
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <div/>,
+    document.getElementById('root') as HTMLElement
+);
+
+registerServiceWorker();
