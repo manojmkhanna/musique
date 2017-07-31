1d089f9e7a689d9bd7abd826f21dd8214f28e8bf
Manoj Khanna <manojkhannakm@gmail.com>
2017-07-31 16:05:08 +0530

    Updated basic layout

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -8,7 +8,7 @@ import "./app.css";
 import "./assets/fonts/material-icons/material-icons.css";
 
 const {MDCToolbar} = require("@material/toolbar");
-const {MDCTabBarScroller, MDCTabBar} = require("@material/tabs");
+const {MDCTabBarScroller} = require("@material/tabs");
 
 export default class App extends React.Component {
     render() {
@@ -17,32 +17,36 @@ export default class App extends React.Component {
                 <header className="mdc-toolbar mdc-toolbar--fixed mdc-toolbar--waterfall">
                     <div className="mdc-toolbar__row">
                         <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
-                            <a className="mdc-toolbar__icon--menu material-icons" href="#">music_note</a>
+                            <a className="mdc-toolbar__icon--menu material-icons" href="#">menu</a>
                             <span className="mdc-toolbar__title">Musique</span>
                         </section>
                         <section className="mdc-toolbar__section mdc-toolbar__section--align-end">
                             <a className="mdc-toolbar__icon material-icons" href="#">settings</a>
+                            <a className="mdc-toolbar__icon material-icons" href="#">more_vert</a>
                         </section>
                     </div>
                     <div className="mdc-toolbar__row">
-                        <div className="mdc-tab-bar-scroller">
-                            <div className="mdc-tab-bar-scroller__indicator mdc-tab-bar-scroller__indicator--back">
-                                <a className="mdc-tab-bar-scroller__indicator__inner material-icons" href="#">navigate_before</a>
+                        <section className="mdc-toolbar__section">
+                            <div className="mdc-tab-bar-scroller">
+                                <div className="mdc-tab-bar-scroller__indicator mdc-tab-bar-scroller__indicator--back">
+                                    <a className="mdc-tab-bar-scroller__indicator__inner material-icons" href="#">navigate_before</a>
+                                </div>
+                                <div className="mdc-tab-bar-scroller__scroll-frame">
+                                    <nav className="mdc-tab-bar mdc-tab-bar-scroller__scroll-frame__tabs">
+                                        <a className="mdc-tab mdc-tab--active" href="#">Search</a>
+                                        <a className="mdc-tab" href="#">Songs</a>
+                                        <a className="mdc-tab" href="#">Albums</a>
+                                        <a className="mdc-tab" href="#">Artists</a>
+                                        <a className="mdc-tab" href="#">Playlists</a>
+                                        <span className="mdc-tab-bar__indicator"/>
+                                    </nav>
+                                </div>
+                                <div
+                                    className="mdc-tab-bar-scroller__indicator mdc-tab-bar-scroller__indicator--forward">
+                                    <a className="mdc-tab-bar-scroller__indicator__inner material-icons" href="#">navigate_next</a>
+                                </div>
                             </div>
-                            <div className="mdc-tab-bar-scroller__scroll-frame">
-                                <nav className="mdc-tab-bar mdc-tab-bar-scroller__scroll-frame__tabs">
-                                    <a className="mdc-tab mdc-tab--active" href="#">Search</a>
-                                    <a className="mdc-tab" href="#">Songs</a>
-                                    <a className="mdc-tab" href="#">Albums</a>
-                                    <a className="mdc-tab" href="#">Artists</a>
-                                    <a className="mdc-tab" href="#">Playlists</a>
-                                    <span className="mdc-tab-bar__indicator"/>
-                                </nav>
-                            </div>
-                            <div className="mdc-tab-bar-scroller__indicator mdc-tab-bar-scroller__indicator--forward">
-                                <a className="mdc-tab-bar-scroller__indicator__inner material-icons" href="#">navigate_next</a>
-                            </div>
-                        </div>
+                        </section>
                     </div>
                 </header>
                 <main className="mdc-toolbar-fixed-adjust">
@@ -63,7 +67,5 @@ export default class App extends React.Component {
         toolbar.fixedAdjustElement = document.querySelector(".mdc-toolbar-fixed-adjust");
 
         MDCTabBarScroller.attachTo(document.querySelector(".mdc-tab-bar-scroller"));
-
-        MDCTabBar.attachTo(document.querySelector(".mdc-tab-bar"));
     }
 }
