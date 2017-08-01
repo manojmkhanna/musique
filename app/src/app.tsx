d64addff1c22327aa7e221b10f2a327b572634ea
Manoj Khanna <manojkhannakm@gmail.com>
2017-08-01 12:18:25 +0530

    Added menu

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import * as mdc from "material-components-web";
 
-import "@material/typography/dist/mdc.typography.css";
-import "@material/toolbar/dist/mdc.toolbar.css";
-import "@material/tabs/dist/mdc.tabs.css";
 import "@material/drawer/dist/mdc.drawer.css";
 import "@material/list/dist/mdc.list.css";
+import "@material/menu/dist/mdc.menu.css";
+import "@material/tabs/dist/mdc.tabs.css";
+import "@material/toolbar/dist/mdc.toolbar.css";
+import "@material/typography/dist/mdc.typography.css";
 
 import "./app.css";
 import "./assets/fonts/material-icons/material-icons.css";
@@ -17,20 +18,19 @@ export default class App extends React.Component {
                 <header className="mdc-toolbar mdc-toolbar--fixed mdc-toolbar--waterfall">
                     <div className="mdc-toolbar__row">
                         <section className="mdc-toolbar__section mdc-toolbar__section--align-start">
-                            <a className="material-icons mdc-toolbar__icon--menu" href="#">menu</a>
+                            <i className="material-icons mdc-toolbar__icon--menu toolbar-drawer-button">menu</i>
                             <span className="mdc-toolbar__title">Musique</span>
                         </section>
                         <section className="mdc-toolbar__section mdc-toolbar__section--align-end">
-                            <a className="material-icons mdc-toolbar__icon" href="#">settings</a>
-                            <a className="material-icons mdc-toolbar__icon" href="#">more_vert</a>
+                            <i className="material-icons mdc-toolbar__icon toolbar-settings-button">settings</i>
+                            <i className="material-icons mdc-toolbar__icon toolbar-menu-button">more_vert</i>
                         </section>
                     </div>
                     <div className="mdc-toolbar__row">
                         <section className="mdc-toolbar__section">
                             <div className="mdc-tab-bar-scroller">
                                 <div className="mdc-tab-bar-scroller__indicator mdc-tab-bar-scroller__indicator--back">
-                                    <a className="material-icons mdc-tab-bar-scroller__indicator__inner tab-bar-scroller-indicator"
-                                       href="#">navigate_before</a>
+                                    <i className="material-icons mdc-tab-bar-scroller__indicator__inner tab-bar-scroller-indicator">navigate_before</i>
                                 </div>
                                 <div className="mdc-tab-bar-scroller__scroll-frame">
                                     <nav className="mdc-tab-bar mdc-tab-bar-scroller__scroll-frame__tabs">
@@ -44,8 +44,7 @@ export default class App extends React.Component {
                                 </div>
                                 <div
                                     className="mdc-tab-bar-scroller__indicator mdc-tab-bar-scroller__indicator--forward">
-                                    <a className="material-icons mdc-tab-bar-scroller__indicator__inner tab-bar-scroller-indicator"
-                                       href="#">navigate_next</a>
+                                    <i className="material-icons mdc-tab-bar-scroller__indicator__inner tab-bar-scroller-indicator">navigate_next</i>
                                 </div>
                             </div>
                         </section>
@@ -77,6 +76,12 @@ export default class App extends React.Component {
                         </nav>
                     </nav>
                 </aside>
+                <div className="mdc-simple-menu mdc-simple-menu--open-from-top-right menu" data-tabindex="-1">
+                    <ul className="mdc-list mdc-simple-menu__items" role="menu">
+                        <li className="mdc-list-item" role="menuitem" data-tabindex="0">Help</li>
+                        <li className="mdc-list-item" role="menuitem" data-tabindex="0">About</li>
+                    </ul>
+                </div>
                 <main className="mdc-toolbar-fixed-adjust">
                     {                                                                                                   //TODO: Remove later
                         new Array(100).fill("").map((value, index) => {
@@ -97,6 +102,9 @@ export default class App extends React.Component {
         mdc.tabs.MDCTabBarScroller.attachTo(document.querySelector(".mdc-tab-bar-scroller"));
 
         let drawer = mdc.drawer.MDCTemporaryDrawer.attachTo(document.querySelector(".mdc-temporary-drawer"));
-        document.querySelector(".mdc-toolbar__icon--menu").addEventListener("click", () => drawer.open = true);
+        document.querySelector(".toolbar-drawer-button").addEventListener("click", () => drawer.open = true);
+
+        let menu = mdc.menu.MDCSimpleMenu.attachTo(document.querySelector(".mdc-simple-menu"));
+        document.querySelector(".toolbar-menu-button").addEventListener("click", () => menu.open = true);
     }
 }
