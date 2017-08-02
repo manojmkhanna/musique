be14f52d46f5a09544d5b6bec4ce51a1032644a9
Manoj Khanna <manojkhannakm@gmail.com>
2017-08-02 11:47:52 +0530

    Updated Drawer

--- app/src/components/Content.tsx
+++ app/src/components/Content.tsx
@@ -2,13 +2,21 @@ import * as React from "react";
 
 import "./Content.css";
 
-export default class Content extends React.Component<any, any> {
+export default class Content extends React.Component<any, any> {    //TODO
     render() {
         return (
             <main className="mdc-toolbar-fixed-adjust">
-                <div id="tab-panels">
+                {/*<div id="tab-panels">*/}
+                {/**/}
+                {/*</div>*/}
 
-                </div>
+                {
+                    new Array(100).fill("").map((value, index) => {
+                        return (
+                            <div key={index} style={{padding: 16}}>{index + 1}</div>
+                        );
+                    })
+                }
             </main>
         );
     }
--- app/src/components/Drawer.tsx
+++ app/src/components/Drawer.tsx
@@ -7,30 +7,22 @@ import "@material/list/dist/mdc.list.css";
 import "./Drawer.css";
 import "../assets/fonts/material-icons/material-icons.css";
 
-export default class Drawer extends React.Component<any, any> { //TODO
+export default class Drawer extends React.Component<any, any> {
     render() {
         return (
             <aside className="mdc-temporary-drawer">
                 <nav className="mdc-temporary-drawer__drawer">
-                    <header className="mdc-temporary-drawer__header" id="drawer-header">
-                        <div className="mdc-temporary-drawer__header-content">
-                            <i className="material-icons" id="drawer-header-icon">music_note</i>
-                            <span className="mdc-typography--headline" id="drawer-header-title">Musique</span>
-                        </div>
-                    </header>
+                    <div className="mdc-temporary-drawer__toolbar-spacer" id="drawer-spacer">
+                        <i className="material-icons" id="drawer-spacer-icon">music_note</i>
+                        <span className="mdc-typography--title" id="drawer-spacer-title">Musique</span>
+                    </div>
                     <nav className="mdc-list-group mdc-temporary-drawer__content">
                         <div className="mdc-list">
                             <a className="mdc-list-item mdc-temporary-drawer--selected" href="#">
-                                <i className="material-icons mdc-list-item__start-detail">music_note</i>Songs
+                                <i className="material-icons mdc-list-item__start-detail">library_music</i>Deezer
                             </a>
                             <a className="mdc-list-item" href="#">
-                                <i className="material-icons mdc-list-item__start-detail">album</i>Albums
-                            </a>
-                            <a className="mdc-list-item" href="#">
-                                <i className="material-icons mdc-list-item__start-detail">person</i>Artists
-                            </a>
-                            <a className="mdc-list-item" href="#">
-                                <i className="material-icons mdc-list-item__start-detail">playlist_play</i>Playlists
+                                <i className="material-icons mdc-list-item__start-detail">library_music</i>Saavn
                             </a>
                         </div>
                     </nav>
